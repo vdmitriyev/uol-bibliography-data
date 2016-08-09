e1341c3b111b8be9f26f78305b380f421c957b15
Viktor Dmitriyev <dmitriyev.viktor@gmail.com>
2016-08-09 12:26:40 +0200

    Adding more URLs to be processed. Autoextracted valid file names as target. List of graduated PhDs.

--- uolbibliography/uolbibliography.py
+++ uolbibliography/uolbibliography.py
@@ -68,22 +68,18 @@ class BSCrawler():
 
         self.logger.info('[i] files will be saved into folder "{0}"'.format(self.work_dir))
 
-    def crawl(self, mergedata, urlfile=None, dispatcher=None):
-        """Method that extracts urls from 'dispatcher' dictionary and process them.
+    def crawl(self, mergedata, urlfile=None):
+        """Method that extracts URLs from given file and process them.
 
         Args:
             urlfile: file that contains URLS to be processed
         """
 
-        if dispatcher:
-            self.dispatcher = dispatcher
-            for url in self.dispatcher:
-                self.logger.info('[i] following URL is going to be parsed:\n {0}'.format(url))
-                self.main_url = url
-                doc = self.download_document(url)
-                self.dispatcher[url](self, doc)
+        self.logger.info('[i] given URls will be processed')
 
+        # data buffer for all processed URLs
         data = []
+
         if urlfile:
              with codecs.open(urlfile, 'r', encoding='utf8') as f_urls:
                 for line in f_urls:
@@ -101,12 +97,21 @@ class BSCrawler():
                     # else:
                     #     self.logger.info('[i] following URL was ignored:\n {0}'.format(stripped))
 
+        # processing graduated PhDs of Computer Science
+        url_graduated = 'http://www.uni-oldenburg.de/informatik/studium-lehre/promotion/promotionen/'
+        sleep(SLEEP_TIME_IN_SECONDS)
+        doc_gradauted = self.download_document(url_graduated)
+        self.process_uol_graduated_phds(doc=doc_gradauted, output_file_name='cs-graduated-phds')
+
+        # merging together all processed data
         if mergedata:
             csv = self.data_as_csv(data)
             self.helper.save_file(os.path.join(self.work_dir, 'uolbibliography-merged.csv'), csv)
 
+        self.logger.info('[i] given URls were processed')
+
     def download_document(self, url):
-        """ Downloading html page and storing inside string.
+        """ Downloading HTML page and storing inside string.
 
         Args:
             url: URL to be downloaded
@@ -123,6 +128,20 @@ class BSCrawler():
             self.logger.error('[e] exception: {0}, arguments: {1}'.format(ex.message, ex.args))
 
         return html
+
+    def get_data_from_table(self, table_body):
+        """ Getting data from HTML table with BS."""
+
+        data = []
+
+        rows = table_body.find_all('tr')
+        for row in rows:
+            cols = row.find_all('td')
+            cols = [ele.text.strip() + " " for ele in cols] # Add extra space for empty values
+            #data.append([elem for ele in cols if ele]) # Get rid of empty values
+            data.append([ele.strip() for ele in cols]) # Take all values
+
+        return data
 
     def validate_file_name(self, file_name):
         """ Removes all symbols that are not file name conform.
@@ -192,11 +211,43 @@ class BSCrawler():
         return row
 
 
+    def process_uol_graduated_phds(self, doc, output_file_name = None):
+        """ Processing given HTML to extract graduated PhDs.
+
+        Args:
+            doc:    document to be processed
+        """
+
+        bs_html = BeautifulSoup(doc, 'html5lib')
+
+        # getting name of the file from HTML
+        if output_file_name is None:
+            output_file_name = self.helper.gen_file_name(extention='')
+
+        # validating
+        output_file_name = self.validate_file_name(output_file_name)
+
+        # getting data from HTML table
+        table = bs_html.find('table', attrs={'class':'farbe_lichtblau breite100'})
+        table_body = table.find('tbody')
+        data = self.get_data_from_table(table_body)
+
+        # data from list to CSV
+        csv_data = ''
+        for row in data:
+            fow_as_csv = '","'.join(row)
+            fow_as_csv = '"' + fow_as_csv + '"' + '\n'
+            csv_data += fow_as_csv
+
+        self.helper.save_file(os.path.join(self.work_dir, output_file_name + '.csv'), csv_data)
+
     def process_uol_bibliography_tbl(self, doc, output_file_name = None):
         """ Processing given HTML to extract publication information from UOL's Hochschulbibliografie.
 
         Args:
             doc:    document to be processed
+        Returns:
+            table as a collection of Python lists
         """
 
         def is_valid_row(data_chunk):
@@ -215,9 +266,6 @@ class BSCrawler():
 
         bs_html = BeautifulSoup(doc, 'html5lib')
 
-        prettified_html = bs_html.prettify()
-        text_from_html = bs_html.get_text()
-
         # getting name of the file from HTML
         if output_file_name is None:
             div_tag = bs_html.find('div', attrs={'id' : 'inhalt', 'class':'floatbox'})
@@ -227,16 +275,13 @@ class BSCrawler():
             else:
                 output_file_name = self.helper.gen_file_name(extention='')
 
+        # validating file name
+        output_file_name = self.validate_file_name(output_file_name)
+
         # getting data from HTML table
-        data = []
         table = bs_html.find('table', attrs={'class':'infotabelle'})
         table_body = table.find('tbody')
-
-        rows = table_body.find_all('tr')
-        for row in rows:
-            cols = row.find_all('td')
-            cols = [ele.text.strip() for ele in cols]
-            data.append([ele for ele in cols if ele]) # Get rid of empty values
+        data = self.get_data_from_table(table_body)
 
         # cleaning table data
         cleaned_data = []
@@ -244,12 +289,21 @@ class BSCrawler():
             if is_valid_row(row):
                 cleaned_data.append(row)
 
-        csv = self.data_as_csv(cleaned_data)
+        csv_data = self.data_as_csv(cleaned_data)
 
         # text data for debugging, uncomment if needed
+        # prettified_html = bs_html.prettify()
         # self.helper.save_file(os.path.join(self.work_dir, output_file_name + '.html'), prettified_html)
+        # text_from_html = bs_html.get_text()
         # self.helper.save_file(os.path.join(self.work_dir, output_file_name + '.txt'), text_from_html)
-        self.helper.save_file(os.path.join(self.work_dir, self.validate_file_name(output_file_name) + '.csv'), csv)
+
+        # get proper file name length
+        target_file = os.path.join(self.work_dir, output_file_name + '.csv')
+        if len(target_file) > 250:
+            target_file = target_file[:250] + '.csv'
+            #self.logger.info(target_file)
+
+        self.helper.save_file(target_file, csv_data)
 
         return cleaned_data
 
