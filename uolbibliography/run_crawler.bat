768d8a45467d6be944da1bd9766a2d72f447f1f3
Viktor Dmitriyev <dmitriyev.viktor@gmail.com>
2017-09-05 10:22:55 +0200

    Updating crawler for the year 2016.

--- uolbibliography/run_crawler.bat
+++ uolbibliography/run_crawler.bat
@@ -1,4 +1,6 @@
 @echo off
 REM @author Viktor Dmitriyev
-python uolbibliography.py --urlfile=uolbibliography-test.txt --mergedata
+call D:\tmp\python\globvenv\Scripts\activate.bat
+REM python uolbibliography.py --urlfile=uolbibliography-test.txt --mergedata
+python uolbibliography.py --urlfile=uolbibliography-full.txt --mergedata
 pause
