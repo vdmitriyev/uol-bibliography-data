cd1135ec9577e52f2703da7e1a9d93c614c4fac8
Viktor Dmitriyev <dmitriyev.viktor@gmail.com>
2018-08-04 04:14:26 +0200

    UPDATE: The citation crawler becomes a possibility to work with crossref.

--- uolbibliography/run_citator.bat
+++ uolbibliography/run_citator.bat
@@ -1,5 +1,9 @@
 @echo off
 REM @author Viktor Dmitriyev
-PATH = c:\Soft\Anaconda3\;%PATH%
+call D:\tmp\python\globvenv\Scripts\activate.bat
+REM PATH = c:\Soft\Anaconda3\;%PATH%
+REM SET PATH=C:\Soft\Anaconda3;C:\Soft\Anaconda3\Scripts;C:\Soft\Anaconda3\Library\bin;%PATH%
+REM conda create -n tests python=3.6 
+REM call activate tests
 python uolbibliography_citator.py --input=generated-2018/uolbibliography-2008-2017-merged-cleaned.csv
 pause
--- uolbibliography/uolbibliography_citator.py
+++ uolbibliography/uolbibliography_citator.py
@@ -9,13 +9,17 @@ __created__     = "03.08.2018"
 __description__ = "Get citations for data of 'Hochschulbibliografie' (Universities Publication Bibliography) of UOL."
 
 import os
+import csv
 import sys
 import uuid
 import json
 import time
+import random
 import shutil
 import argparse
-import pandas as pd
+#import pandas as pd
+
+from habanero import Crossref
 
 # helpers
 import helpers as hlp
@@ -39,6 +43,39 @@ finally:
 # settings
 CITATIONS_DIR = 'citations'
 CITATIONS_JSONDB_NAME = 'citations-db.json'
+
+import codecs, cStringIO
+
+class UTF8Recoder:
+    """
+    Iterator that reads an encoded stream and reencodes the input to UTF-8
+    """
+    def __init__(self, f, encoding):
+        self.reader = codecs.getreader(encoding)(f)
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        return self.reader.next().encode("utf-8")
+
+class UnicodeReader:
+    """
+    A CSV reader which will iterate over lines in the CSV file "f",
+    which is encoded in the given encoding.
+    """
+
+    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
+        f = UTF8Recoder(f, encoding)
+        self.reader = csv.reader(f, dialect=dialect, **kwds)
+
+    def next(self):
+        row = self.reader.next()
+        return [unicode(s, "utf-8") for s in row]
+
+    def __iter__(self):
+        return self
+
 
 class UOLBibliographyCitator:
     """ Get citations for data of 'Hochschulbibliografie' ((Universities Publication Bibliography) of UOL. """
@@ -71,9 +108,70 @@ class UOLBibliographyCitator:
             with open(path_citations_db) as json_data:
                 citations_db = json.load(json_data)
         except Exception as ex:
-            print ('Exception happened: {0}'.format(str(ex)))
+            print ('Exception happened: {0}'.format(str (ex)))
 
         return citations_db
+
+    def read_csv(self, f_input):
+        ''' Read data from CSV'''
+
+        # the pandas way
+        #data = pd.read_csv(f_input, sep=',')
+
+        # the vanilla way
+        data = []
+        with open(f_input, 'rb') as csvfile:
+
+            # no UTF-8
+            #csv_reader = csv.reader(csvfile, delimiter=',')
+            #csv_reader = csv.reader(utf_8_encoder(unicode_csv_data),**kwargs)
+            # for index, row in enumerate(csv_reader):
+            #     if index > 0:
+            #         data.append(row)
+
+            csv_reader = UnicodeReader(csvfile)
+            for index, row in enumerate(csv_reader):
+                if index > 0:
+                    data.append(row)
+
+        return data
+
+    def citation_via_scholar(self, querier, row):
+        ''' Get citation from Google Scholar'''
+
+        result = None
+
+        try:
+            # prepare scholar query
+            query = scholar.SearchScholarQuery()
+            query.set_author(row[1]) # author name
+            query.set_words(row[2])  # publication title
+            querier.send_query(query)# send query
+
+            # save citation as JSON
+            result = {'source': 'GS',
+                      'value': querier.articles[0]['num_citations']}
+        except Exception as ex:
+            print('[e] Exception while getting number of citations (Google Scholar): {0}.\nTitle:{1}'.format(ex, row[2]))
+
+        return result
+
+    def citation_via_crossref(self, cr, row):
+        ''' Get citation from Crossref'''
+
+        result = None
+        query = row[2] + ' ' + row[1]
+
+        try:
+            response = cr.works(query = query, limit = 1)
+            if response['message']['items']:
+                result = {'source': 'CR',
+                          'value': response['message']['items'][0]['is-referenced-by-count']}
+        except Exception as ex:
+            print('[e] Exception while getting number of citations (Crossref): {0}.\nTitle:{1}'.format(ex, row[2]))
+
+        return result
+
 
     def crawl_citations(self, f_input):
         """ Crawls citations for each publication.
@@ -86,39 +184,40 @@ class UOLBibliographyCitator:
             os.makedirs(CITATIONS_DIR)
 
         # load data
-        df_original = pd.read_csv(f_input, sep=',')
+        df_original = self.read_csv(f_input)
         citations_db = self.load_citations()
 
         # prepare scholar crawler
         querier = scholar.ScholarQuerier()
         settings = scholar.ScholarSettings()
         querier.apply_settings(settings)
-        crawls_cnt = 0
 
-        for index, row in df_original.iterrows():
+        # prepare Crossref
+        cr = Crossref()
+
+        #for index, row in df_original.iterrows(): # pandas way
+        for index, row in enumerate(df_original):
 
             if row[2] not in citations_db:
 
-                crawls_cnt+=1
-                citations_db[row[2]] = 0
+                #crawls_cnt+=1
+                citations_db[row[2]] = {}
 
-                # prepare scholar query
-                query = scholar.SearchScholarQuery()
-                query.set_author(row[1]) # author name
-                query.set_words(row[2])  # publication title
-                querier.send_query(query)# send query
+                # citation = self.citation_via_scholar(querier, row)
 
-                # save citation into JSON
-                try:
-                    citations_db[row[2]] = querier.articles[0]['num_citations']
-                except Exception as ex:
-                    citations_db[row[2]] = -1
-                    print('[e] Exception while getting number of citations: {0}.\nTitle:{1}'.format(ex, row[2]))
+                # if citation is not None:
+                #     citations_db[row[2]] = citation
+                # else:
+
+                citation = self.citation_via_crossref(cr, row)
+
+                citations_db[row[2]] = citation
 
             # save crawled DB after N titles processed and sleep
-            if index % 25 == 0:
-                print ('[i] Save intermediate results and sleep. Total processed so far: {0}'.format(index))
-                time.sleep(1)
+            if index % 15 == 0:
+                sleep_interval = random.randrange(5, 13, 1)
+                print ('[i] Save intermediate results and sleep for {0}. Total processed so far: {1}'.format(sleep_interval, index))
+                time.sleep(sleep_interval)
                 self.dump_citations(citations_db)
 
 def main(input):
